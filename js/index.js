5e869967be75e3bde7354eab00b2616917b516b9
Antiky <31079363+Antiky@users.noreply.github.com>
2018-06-10 11:07:14 +0800

    add my

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var dappAddress = "n1qBXaNhfZoBAfbpkvPMwxcKP9S9eh7Gste";
 $(function () {
-	var NebPay = require("nebpay"); 
+	var NebPay = require("nebpay");
 	var nebpay = new NebPay();
 
 	$("#all").click(function () {
@@ -24,16 +24,19 @@ $(function () {
 				var tempStr = "";
 				var time = Math.round(new Date().getTime() / 1000).toString(); //获取当前时间戳
 
-				for (var i = 0; i < res.length; i++) {
-					if (i % 2 == 0) {  //0 2 4 6 8
+				for (var i = 0; i < res.length-1; i++) {
+					if (i % 2 == 0) { //0 2 4 6 8
 						tempStr += '<div class="panel-body"> ';
-					} else {		//1 3 4 5 6
+					} else { //1 3 4 5 6
 						tempStr += '<div class="panel-footer">';
 					}
 					tempStr += '<p>';
 					tempStr += res[i].index + " " + res[i].content;
 					tempStr += '<br>';
-					tempStr += '<small><cite>' + 'Author:' + res[i].author.substr(3,6) + '</cite></small>';
+					tempStr += '<small><cite>' + 'ID:' + res[i].author.substr(3, 6) + '</cite></small>';
+					tempStr += '<a class="btn" href="javascript:void(0)" id="like" onclick="addMy(';
+					tempStr += res[i].index;
+					tempStr += ')">联系TA</a>';
 					tempStr += '</p> </div><hr> ';
 				}
 				console.log(tempStr);
@@ -42,120 +45,122 @@ $(function () {
 		});
 	});
 
-$("#create").click(function () {
-	$("#detailTitle").text("自我介绍")
-	var tempStr = '';
-	tempStr += '<div class="panel-body"> ';
-	tempStr += '<form role="form">';
-	tempStr += '<div class="form-group">';
-	tempStr += '<br><h6>公开资料</h6>';
-	tempStr += '<textarea class="form-control1" rows="10" id="content" placeholder="姓名&#13;&#10;性别&#13;&#10;个人介绍&#13;&#10;所在区域"></textarea>';
-	tempStr += '<h6>隐私资料</h6>';
-	tempStr += '<input type="text" class="form-control" rows="1" id="name" placeholder="联系电话">';
-	tempStr += '<p>收取0.00001NAS保证金</p>';
-	tempStr += '<br><br><button class="button1" class="btn btn-primary" id="savebutton" onclick="save();">上传信息</button><p color="red";>一旦提交将不可修改</p>';
-	tempStr += '</div>';
-	tempStr += '</form>';
-	tempStr += '</div> ';
-	console.log(tempStr);
-	$("#searchresult").html(tempStr);
-});
+	$("#my").click(function () {
+		$("#detailTitle").text("约会历史");
 
-$("#help").click(function () {
-	$("#detailTitle").text("Help");
-	var tempStr = '';
-	tempStr += '<p>您已约会2人</p>';
-	console.log(tempStr);
 
-	$("#searchresult").html(tempStr);
-});
 
-$("#about").click(function () {
-	$("#detailTitle").text("about");
-	var tempStr = '';
-	tempStr += '<p>10000 NAS</p>';
-	console.log(tempStr);
+		var to = dappAddress;
+		var value = "0";
+		var callFunction = "getMy";
+		var callArgs = "[]";
+		nebpay.simulateCall(to, value, callFunction, callArgs, {
+			listener: function (resp) {
+				//console.log(JSON.stringify(resp.result));
+				if (resp.result == "") {
+					$("#searchresult").html('<div class="panel-body">暂时没有记录</div>');
+					return;
+				}
+				var res = JSON.parse(resp.result);
+				if (res.length == 0) {
+					$("#searchresult").html('<div class="panel-body">暂时没有记录</div>');
+					return;
+				}
 
-	$("#searchresult").html(tempStr);
-});
 
-	$("#updatee").click(function () {
-		$("#detailTitle").text("updatee");
+				var tempStr = "";
+
+				for (var i = 0; i < res.length; i++) {
+					if (i % 2 == 0) {
+						tempStr += '<div class="panel-body"> ';
+					} else {
+						tempStr += '<div class="panel-footer">';
+					}
+
+					tempStr += '<p>';
+					tempStr += res[i].index + " " + res[i].content;
+					tempStr += '<br>';
+					tempStr += '<small><cite>' + 'ID:' + res[i].author.substr(3, 6) + '</cite></small>';
+					tempStr += '<br>联系方式:' + res[i].contact;
+					tempStr += '</p> </div><hr> ';
+				}
+				console.log(tempStr);
+				$("#searchresult").html(tempStr);
+			}
+		});
+
+	});
+
+	$("#create").click(function () {
+		$("#detailTitle").text("自我介绍")
 		var tempStr = '';
 		tempStr += '<div class="panel-body"> ';
 		tempStr += '<form role="form">';
 		tempStr += '<div class="form-group">';
-		tempStr += '<p>商品序号</p>';
-		tempStr += '<textarea class="form-control" rows="1" id="nids" >0</textarea>';
-		tempStr += '<p>出价 (加价至少0.001)</p>';
-		tempStr += '<textarea class="form-control" rows="1" id="nidsmon" >0.001</textarea>';
-		tempStr += '<button type="button" class="btn btn-primary" id="savebidbutton" onclick="savebid();">请点击列表中的"参与拍卖"按钮</button>';
+		tempStr += '<br><h6>公开资料</h6>';
+		tempStr += '<textarea class="form-control1" rows="10" id="content" placeholder="姓名&#13;&#10;性别&#13;&#10;个人介绍&#13;&#10;所在区域"></textarea>';
+		tempStr += '<h6>隐私资料</h6>';
+		tempStr += '<input type="text" class="form-control" rows="1" id="name" placeholder="联系电话">';
+		tempStr += '<p>收取0.00001NAS保证金</p>';
+		tempStr += '<br><br><button class="button1" class="btn btn-primary" id="savebutton" onclick="save();">上传信息</button><p color="red";>一旦提交将不可修改</p>';
 		tempStr += '</div>';
 		tempStr += '</form>';
 		tempStr += '</div> ';
 		console.log(tempStr);
-
 		$("#searchresult").html(tempStr);
 	});
+
+	$("#about").click(function () {
+		$("#detailTitle").text("关于")
+		var tempStr = '';
+		tempStr += '<div class="panel-body"> ';
+		tempStr += '<form role="form">';
+		tempStr += '<div class="form-group">';
+		tempStr += '<h6>关于</h6>';
+		tempStr += '<p>SegmentFault Blockchain Hackathon 编程马拉松 2018 北京站 参赛作品</p>';
+		tempStr += '<div align="center" class="banner"><img src="image/about.jpg" alt=""></div>'
+		tempStr += '</div>';
+		tempStr += '</form>';
+		tempStr += '</div> ';
+		console.log(tempStr);
+		$("#searchresult").html(tempStr);
+	});
+
+
 });
 
+function addMy(index) {
+	var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
+	var nebpay = new NebPay();
+	var to = dappAddress;
+	var value = "0.00001"; //保证金
+	var callFunction = "addMy";
+	var callArgs = "[\"" + index + "\"]";
+	nebpay.call(to, value, callFunction, callArgs, {
+		listener: function (resp) {
+			console.log(JSON.stringify(resp.result));
+		}
+	});
+};
 
-
-function bidinfo(i) {
-    $("#detailTitle").text("updatee");
+function reMy(index) {
 	var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
 	var nebpay = new NebPay();
 	var to = dappAddress;
 	var value = "0";
-	var callFunction = "getData";
-	var callArgs = "[]";
-
-
-	nebpay.simulateCall(to, value, callFunction, callArgs, {
+	var callFunction = "reMy";
+	var callArgs = "[\"" + index + "\"]";
+	nebpay.call(to, value, callFunction, callArgs, {
 		listener: function (resp) {
-			//console.log(JSON.stringify(resp.result));
-			if (resp.result == "") {
-				$("#searchresult").html('<div class="panel-body" >nodata</div>');
-				return;
-			}
-			var res = JSON.parse(resp.result);
-			if (res.length == 0) {
-				$("#searchresult").html('<div class="panel-body">nodata</div>');
-				return;
-			}
-			var tempStr = "";
-			cj = res[i].cjvalue / (10e17);
-			if (i % 2 == 0) {  //0 2 4 6 8
-				tempStr += '<div class="panel-body"> ';
-			} else {		//1 3 4 5 6
-				tempStr += '<div class="panel-footer">';
-			}
-
-			tempStr += '<div class="xh">';
-			tempStr += "ID:" + res[i].index  + "<br> 内容:"  + res[i].content;
-			tempStr += '</div>';
-			tempStr += '<div class="info">';
-			tempStr += '<small><cite>' + 'Principal seller:' + res[i].author + '</cite></small>';
-			tempStr += '<br>';
-			tempStr += '<small><cite>' + 'Start Time:' + ttt(res[i].createdDate) + '</cite></small>';
-			tempStr += '</div>';
-			tempStr += '<div class="cj">';
-			tempStr += 'Bid increments 0.001 NAS';
-			tempStr += '<br><input type="text" class="form-control" rows="1" id="nidsmon"  placeholder="0.001">';
-			tempStr += '<br><button type="button" class="btn btn-primary" id="savebidbutton" onclick="savebid(' + i + ');">BID</button>';
-			tempStr += '</div>';
-			tempStr += '</form>';
-			tempStr += '</div> ';
-
-			$("#searchresult").html(tempStr);
+			console.log(JSON.stringify(resp.result));
 		}
 	});
-}
+};
 
 function savebid(i) {
 	var NebPay = require("nebpay"); //https://github.com/nebulasio/nebPay
 	var nebpay = new NebPay();
-	var nids = i.toString();   
+	var nids = i.toString();
 	var nidsmon = $("#nidsmon").val();
 	if (nids == "") {
 		alert("FAIL...");
@@ -215,27 +220,6 @@ function save() {
 	});
 };
 
-function conbid(i) {
-	var NebPay = require("nebpay"); 
-	var nebpay = new NebPay();
-	var ids = i.toString();
-	if (ids == "") {
-		alert("FAIL...");
-		return;
-	}
-	ids = ids.replace(/\n/g, "<br>");
-	nebpay.call(dappAddress, "0", "con", "[\"" + ids + "\"]", {
-		listener: function Push(resp) {
-			console.log("response of push: " + JSON.stringify(resp))
-			var respString = JSON.stringify(resp);
-			if (respString.search("rejected by user") !== -1) {
-				alert("cancle")
-			} else if (respString.search("txhash") !== -1) {
-				alert("Update Hash: " + resp.txhash)
-			}
-		}
-	})
-};
 
 function ttt(timestamp) {
 	var date = new Date(timestamp * 1000);
